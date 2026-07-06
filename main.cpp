9e7d6af172eaba9fa7e41e32c931f31fdeec6859
godkiller10 <tahmidahnaf641@gmail.com>
2026-07-06 15:56:51 +0600

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    // ==========================
+    // Student Information
+    // ==========================
+
+    string name = "Ahnaf Tahmid Zaman";
+    string id = "23-52193-2";
+    string section = "X";
+
+    // ==========================
+    // Personal Information
+    // ==========================
+
+    string department = "CSE";
+    string university = "AIUB";
+
+    // ==========================
+    // Display
+    // ==========================
+
+    cout << "==============================" << endl;
+    cout << " Student Information System" << endl;
+    cout << "==============================" << endl;
+
+    cout << "Name       : " << name << endl;
+    cout << "ID         : " << id << endl;
+    cout << "Section    : " << section << endl;
+    cout << "Department : " << department << endl;
+    cout << "University : " << university << endl;
+
+    cout << "\nWelcome to Git and GitHub!" << endl;
+
+    return 0;
+}
